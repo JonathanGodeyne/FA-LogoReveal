ceafd7ead839c42aceab599bdbf8cf1d79a5a365
JonathanGodeyne <11307408@student.pxl.be>
2022-04-12 16:12:33 +0200

    added

---
+++ ScreenSelector.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Febelfin_academy_Logo_reveal
+{
+    public partial class ScreenSelector : Window
+    {
+        public ScreenSelector()
+        {
+            InitializeComponent();
+
+            
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void GetScreens()
+        {
+            //ComboboxItem
+        }
+    }
+}
---
+++ Screenhelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Febelfin_academy_Logo_reveal
+{
+    public static class Screenhelper
+    {
+        public static Screen[] allScreens { get { return Screen.AllScreens; } }         
+    }
+}
